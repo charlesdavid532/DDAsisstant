d2582986575648571b6e5a501a3802bc34996ca8
David <cdavid@deloitte.com>
2017-08-10 22:43:35 +0530

    Change json

--- app.py
+++ app.py
@@ -106,10 +106,24 @@ def makeWebhookResult(data):
     
 
 def makeCard(resp):
-
+    '''
     resp.card(text='Dummy Card',title='Card title',img_url='https://drive.google.com/open?id=0BzU--BJmmVjua0dSVnZNYVJCLXc')
 
     return resp
+    '''
+    return {
+        "simpleResponse": {
+            "textToSpeech": "Math and prime numbers it is!"
+        },
+        "basicCard": {
+            "title": "Math & prime numbers",
+            "formattedText": "42 is an even composite number. It \n      is composed of three distinct prime numbers multiplied together. It \n      has a total of eight divisors. 42 is an abundant number, because the \n      sum of its proper divisors 54 is greater than itself. To count from \n      1 to 42 would take you about twenty-one…",
+            "image": {
+                "url": "https://www.google.com/search?q=42",
+                "accessibilityText": "Image alternate text"
+        },
+        "buttons": []
+    }
 
 def getParameters(req):
     result = req.get("result")
