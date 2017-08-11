cdb278fa88751f454930547e0ab414c4f8ec424f
David <cdavid@deloitte.com>
2017-08-11 17:48:25 +0530

    Added querying from database

--- app.py
+++ app.py
@@ -137,6 +137,19 @@ def makeCard(resp):
          "source": "DDAsisstant"
     }
     '''
+
+    # This is the database query to fetch the appropriate data (TODO: Move to another function)
+    tempData = mongo.db.temp1
+    try: 
+        for s in tempData.find({'email': 'cdavid@deloitte.com'}):
+            fullName = s['name']
+            print ("The name is:" + s['name'])
+            designation = s['designation']
+
+    except Exception:
+        print("Could not query database")
+        return ''
+
     return {
         "speech": "Howdy",
         "displayText": "Howdy",
@@ -147,42 +160,33 @@ def makeCard(resp):
           "items": [
             {
               "simpleResponse": {
-                  "textToSpeech":"This is the first simple response for a basic card"
+                  "textToSpeech":"Here are all the employees of Deloitte Digital"
               }
             },
             {
               "basicCard": {
-                "title":"Title: this is a title",
-                "formattedText":"This is a basic card.  Text in a\n      basic card can include \"quotes\" and most other unicode characters\n      including emoji 📱.  Basic cards also support some markdown\n      formatting like *emphasis* or _italics_, **strong** or __bold__,\n      and ***bold itallic*** or ___strong emphasis___ as well as other things\n      like line  \nbreaks",
-                "subtitle":
-                "This is a subtitle",
+                "title":fullName,
+                "formattedText":"Charles is an MBA from NMIMS",
+                "subtitle":designation,
                 "image": {
                   "url":"https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
                   "accessibilityText":"Image alternate text"
                 },
                 "buttons": [
                   {
-                    "title":"This is a button",
+                    "title":"View Profile",
                     "openUrlAction":{
                       "url":"https://assistant.google.com/"
                     }
                   }
                 ]
               }
-            },
-            {
-              "simpleResponse": {
-                "textToSpeech":"This is the 2nd simple response ",
-                "displayText":"This is the 2nd simple response"
-              }
             }
           ],
           "suggestions":
           [
-            {"title":"Basic Card"},
-            {"title":"List"},
-            {"title":"Carousel"},
-            {"title":"Suggestions"}
+            {"title":"I have a new project"},
+            {"title":"Search people with skills"}
           ]
         }
         }
