fe5b25ca945a3bc57ce30b22d9bed8031a40b3c2
David <cdavid@deloitte.com>
2017-08-17 09:18:21 +0530

    Change url of image to local file

--- app.py
+++ app.py
@@ -152,13 +152,13 @@ def makeCard(resp):
     
     print("Before opening image")
     #added comment
-    createImage()
+    #createImage()
 
     print("Before printing list item")
     #print(json.dumps(createListItem(fullName,fullName,designation,"https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png"), indent=4))
     #print(json.dumps(createListResponse("My sample response",["sug1","sug2"],"My list title",[fullName, "Charlie"],[fullName, "Dans"],[designation, "Cons"],["https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png","https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png"]), indent=4))
 
-    return createListResponse("My sample response",["sug1","sug2"],"My list title",[fullName, "Charlie"],[fullName, "Dans"],[designation, "Cons"],["https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png","https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png"])
+    return createListResponse("My sample response",["sug1","sug2"],"My list title",[fullName, "Charlie"],[fullName, "Dans"],[designation, "Cons"],["profile.jpg","https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png"])
     '''
     return {
         "speech": "Howdy",
